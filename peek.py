b16566da1f3ac342f1f6246e8af7906c50fc98ca
Andy2887 <yuanliheng05@gmail.com>
2026-03-11 09:38:58 -0500

    updated peek.py

--- peek.py
+++ peek.py
@@ -3,12 +3,11 @@ import pyarrow.parquet as pq
 from pyarrow import fs
 
 def main():
-    if len(sys.argv) != 3:
-        print(f"Usage: {sys.argv[0]} <s3_url> <num_rows>")
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <s3_url>")
         sys.exit(1)
 
     s3_url = sys.argv[1]
-    num_rows = int(sys.argv[2])
 
     # Parse s3://bucket/key
     path = s3_url.replace("s3://", "", 1)
@@ -16,15 +15,16 @@ def main():
 
     parquet_file = pq.ParquetFile(s3.open_input_file(path))
     col_name = parquet_file.schema.names[0]
+    col_index = 0
 
-    rows_read = 0
-    for batch in parquet_file.iter_batches(columns=[col_name]):
-        col = batch.column(0)
-        for i in range(len(col)):
-            if rows_read >= num_rows:
-                return
-            print(col[i].as_py())
-            rows_read += 1
+    num_row_groups = min(20, parquet_file.metadata.num_row_groups)
+    for i in range(num_row_groups):
+        row_group_metadata = parquet_file.metadata.row_group(i)
+        col_metadata = row_group_metadata.column(col_index)
+        print(f"--- Row Group {i} ---")
+        print("Min: ", col_metadata.statistics.min)
+        print("Max: ", col_metadata.statistics.max)
+        print()
 
 if __name__ == "__main__":
     main()
